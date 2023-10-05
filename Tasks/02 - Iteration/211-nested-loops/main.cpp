2c21cdaf7b38107fbc05f06e6f92b95cc075177e
Alex-T-Draper <alex@logicalsolutions-av.co.uk>
2023-10-05 10:41:45 +0100

    Finished upto multiplication table
    Added solutions to the files, got upto the multiplication table. 05/10/2023

--- Tasks/02 - Iteration/211-nested-loops/main.cpp
+++ Tasks/02 - Iteration/211-nested-loops/main.cpp
@@ -11,7 +11,7 @@ int main()
         for (int j = 1; j <= 5; j++)    // Inner loop executes 5 times for each outer loop run so 10 time in total
         {
             printf("\tInner loop number %d\n\t\t'i', 'j', 'k'\n", j);  // note the use of the tab (\t) option at the start to improve the layout
-            
+
             //Task - Write another level of nesting here
             for (int k = 1; k <= 3; k++)
             {
